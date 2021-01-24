fc1cddfdf179426aaae82b2dc0d358bbd44cdc95
paolap.98 <paolapetitti123@gmail.com>
2021-01-24 14:11:19 -0500

    CW: removed setup and draw since they aren't used and finished the video

--- classwork/first-class-functions/js/script.js
+++ classwork/first-class-functions/js/script.js
@@ -7,20 +7,6 @@ Paola Petitti
 Following along the first class functions video.
 **************************************************/
 
-// setup()
-//
-// Description of setup() goes here.
-function setup() {
-
-}
-
-// draw()
-//
-// Description of draw() goes here.
-function draw() {
-
-}
-
 setTimeout(function(){
   alert(`Hello!`);
 },5000);
