28c5eb21d041c3a5e3ab727c3201ae663ab596ba
paolap.98 <paolapetitti123@gmail.com>
2021-01-24 15:04:33 -0500

    A1: Added for loop in preload to load animal images

--- activities/sausage-dog-activity/js/script.js
+++ activities/sausage-dog-activity/js/script.js
@@ -6,11 +6,19 @@ Paola Petitti
 
 Following along Pippin's video for the first activity.
 **************************************************/
+const NUM_ANIMAL_IMG = 10;
+const NUM_ANIMALS = 100;
+
+let animalImages = [];
+let animals = [];
 
 // preload()
 // Description of preload
-functino preload(){
-
+function preload(){
+  for (let i = 0; i < NUM_ANIMAL_IMG; i++){
+    let animalImage = loadImage(`assets/images/animal${i}`);
+    animalImages.push(animalImage);
+  }
 }
 
 // setup()
