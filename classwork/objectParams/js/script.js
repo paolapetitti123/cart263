bde7c528135aaeb28a53465a39d7426ca07c8060
paolap.98 <paolapetitti123@gmail.com>
2021-01-24 13:25:22 -0500

    CW: Finished the object parameters video

--- classwork/objectParams/js/script.js
+++ classwork/objectParams/js/script.js
@@ -11,12 +11,34 @@ Following along the Object Parameters video.
 //
 // Description of setup() goes here.
 function setup() {
-
+  createCanvas(500,500);
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
+  background(0);
 
+  let config = {
+    x: 250,
+    y: 250,
+    width: 200,
+    height: 200,
+    fillColor: {
+      r: 255,
+      g: 255,
+      b: 0
+    },
+    mode: CENTER
+  };
+  drawFancyRect(config);
+}
+
+function drawFancyRect({x,y,width,height,fillColor,mode}) {
+  push();
+  fill(fillColor.r,fillColor.g,fillColor.b);
+  rectMode(mode);
+  rect(x,y,width,height);
+  pop();
 }
