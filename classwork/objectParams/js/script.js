45939187f53f302472492894452d50fa13a6c8c6
paolap.98 <paolapetitti123@gmail.com>
2021-01-24 12:02:56 -0500

    CW: Starting object params video

---
+++ classwork/objectParams/js/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/**************************************************
+Object Parameters video
+Paola Petitti
+
+Following along the Object Parameters video.
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
