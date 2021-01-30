453d0ea16857e2c01425bf8a3d3d3fa7243f75be
paolap.98 <paolapetitti123@gmail.com>
2021-01-30 15:24:47 -0500

    CW: finished annyang video

--- classwork/annyang/js/script.js
+++ classwork/annyang/js/script.js
@@ -1,22 +1,50 @@
 "use strict";
 
 /**************************************************
-Template p5 project
-Pippin Barr
+Annyang
+Paola Petitti
 
-Here is a description of this template p5 project.
+Following the annyang video
 **************************************************/
+// The program's face
+let face = `:-|`;
 
-// setup()
-//
-// Description of setup() goes here.
 function setup() {
-
+  createCanvas(500, 500);
+  // Check if annyang is available
+  if (annyang) {
+    // Create commands
+    let commands = {
+      // They love me!
+      'I love you': love,
+      // They hate me!
+      'I hate you': hate
+    }
+    // Add the commands and start annyang
+    annyang.addCommands(commands);
+    annyang.start();
+  }
 }
 
-// draw()
-//
-// Description of draw() goes here.
 function draw() {
+  background(0);
 
+  // Draw the current face emoji in the center of the canvas
+  // rotated to display more like a regular face
+  push();
+  translate(width / 2, height / 2);
+  rotate(PI / 2);
+  textSize(400);
+  textAlign(CENTER, CENTER);
+  fill(255);
+  text(face, 0, 0);
+  pop();
+}
+
+function love() {
+  face = `:-)`;
+}
+
+function hate() {
+  face = `:-(`;
 }
