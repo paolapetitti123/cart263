30a5f4600de893da7ee373c28c393cea597a29b9
paolap.98 <paolapetitti123@gmail.com>
2021-01-31 13:01:09 -0500

    A2: Started Slamina Activity

---
+++ activities/slamina-activity/js/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/**************************************************
+Slamina Activity
+Paola Petitti
+
+Following along the Slamina Activity video
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
