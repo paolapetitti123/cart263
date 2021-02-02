79467ee1ed44dcbf88bcd2335f844e73ec976b86
paolap.98 <paolapetitti123@gmail.com>
2021-02-02 14:35:23 -0500

    E2: Got restart working, added comments, and made sure to restart the gamepoints when the game restarts

--- exercises/slamina-exercise/js/script.js
+++ exercises/slamina-exercise/js/script.js
@@ -6,7 +6,7 @@ Paola Petitti
 
 Adding onto the Slamina activity.
 **************************************************/
-// Animal Names List/Array
+// Animal Names List/Array Variables
 const animals = [
       "aardvark",
       "alligator",
@@ -146,18 +146,19 @@ const animals = [
 let currentAnimal = ``;
 let currentAnswer = ``;
 
+// Game State variables
 let gameState = `start`;
-
-let introInstructions = `You have 30 seconds to name as many animals as you can.
+let introInstructions = `
+You have 30 seconds to name as many animals as you can.
 Press Enter to start!`;
-
 let gamePoints = 0;
 let timer = 30;
 
 
 // setup()
 //
-// Description of setup() goes here.
+// Canvas gets created, annyang gets initialized so that the user can guess
+// the animal names and then the text size, style and alignment are initialized
 function setup() {
   createCanvas(windowWidth,windowHeight);
   if (annyang){
@@ -175,7 +176,7 @@ function setup() {
 
 // draw()
 //
-// Description of draw() goes here.
+// Adds a pink background, and adds different states for the game
 function draw() {
   background(255,100,100);
 
@@ -188,9 +189,13 @@ function draw() {
   else if(gameState === `end`){
     endScreen();
   }
+  console.log(gameState);
 }
 
-
+// rpvAnimal()
+//
+// rpvAnimal means responsiveVoice Animal, this gets RPV to say an animal name
+// in reverse whenever it is called
 function rpvAnimal(){
   currentAnimal = random(animals);
 
@@ -199,23 +204,36 @@ function rpvAnimal(){
   responsiveVoice.speak(reverseAnimal);
 }
 
+// guessAnimal()
+//
+// this turns the users answer into all lowercase to match the animal list array
 function guessAnimal(animal){
   currentAnswer = animal.toLowerCase();
 }
 
+
+// keyPressed()
+//
+// This function changes the state from start to game and also from end to start
+// if the user wants to play again. It also restarts the timer if you want to replay.
 function keyPressed(){
   if(gameState === `start` && keyCode === ENTER){
     gameState = `game`;
     rpvAnimal();
   }
   if(gameState === `end` && keyCode === ENTER){
+    console.log("IM HERE");
     gameState = `game`;
     timer = 30;
+    gamePoints = 0;
     rpvAnimal();
   }
 }
 
-
+// reverseString()
+//
+// This function reverses the animal names by splitting the word up by letter
+// and rewritting them backwards so the RPV can read it backwards.
 function reverseString(string) {
   // Split the string into an array of characters
   let characters = string.split('');
@@ -227,11 +245,36 @@ function reverseString(string) {
   return result;
 }
 
+// startScreen()
+//
+// This function provides the start game message to let users know how to start
+// and how long they have
 function startScreen(){
   fill(0);
   text(introInstructions, width/2,height/2);
 }
 
+// gameTimer()
+//
+// This function adds a timer to the game that lasts 30 seconds and makes the
+// game end once the 30 seconds are up.
+function gameTimer(){
+  textAlign(CENTER);
+  textSize(25);
+  text(timer, 50,50);
+  if(frameCount % 60 == 0 && timer > 0){
+    timer--;
+  }
+  if(timer == 0){
+    gameState = `end`;
+  }
+}
+
+// gameScreen()
+//
+// This starts the timer, and checks to see if the user is getting the right
+// answer or not, once they get the right answer then they get a point and
+// the next animal name gets said.
 function gameScreen(){
   gameTimer();
   console.log(currentAnimal);
@@ -249,24 +292,14 @@ function gameScreen(){
 
 }
 
+// endScreen()
+//
+// This function gives the user the end screen with the number of points they
+// accumulated throughout the game and tells the user they can play again.
 function endScreen(){
   let endMessage = `You got ${gamePoints} right!
   Good job!
   Press Enter to try again!`;
   fill(0);
   text(endMessage, width/2,height/2);
-}
-
-
-function gameTimer(){
-  textAlign(CENTER);
-  textSize(25);
-  text(timer, 50,50);
-  if(frameCount % 60 == 0 && timer > 0){
-    timer--;
-  }
-
-  setTimeout(function(){
-    gameState = `end`;
-  }, 30000);
 }
