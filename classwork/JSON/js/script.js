4c59fec11fe994f3fa458ed5c6f02465bb6b8645
paolap.98 <paolapetitti123@gmail.com>
2021-02-08 19:10:08 -0500

    CW: Started JSON Video

---
+++ classwork/JSON/js/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/**************************************************
+4.1. JSON
+Paola Petitti
+
+Following along the JSON video 
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
