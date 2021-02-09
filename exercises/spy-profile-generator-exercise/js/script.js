a8be2c5947c0aeb44303cc9d4dc2acd59bb8e383
paolap.98 <paolapetitti123@gmail.com>
2021-02-09 11:58:36 -0500

    E3: Starting Spy Profile Exercise

---
+++ exercises/spy-profile-generator-exercise/js/script.js
@@ -0,0 +1,94 @@
+"use strict";
+
+/**************************************************
+4.3. Activity: Spy Profile Generator
+Paola Petitti
+
+Following along Pippin's video for this activity.
+**************************************************/
+
+let spyProfile = {
+  name: `REDACTED`,
+  alias: `REDACTED`,
+  secretWeapon: `REDACTED`,
+  password: `REDACTED`
+};
+
+let instrumentData = undefined;
+let objectData = undefined;
+let tarotData = undefined;
+
+/**
+Description of preload()
+*/
+function preload(){
+  tarotData = loadJSON(`https://raw.githubusercontent.com/dariusk/corpora/master/data/divination/tarot_interpretations.json`);
+  instrumentData = loadJSON(`https://raw.githubusercontent.com/dariusk/corpora/master/data/music/instruments.json`);
+  objectData = loadJSON(`https://raw.githubusercontent.com/dariusk/corpora/master/data/objects/objects.json`);
+}
+
+
+/**
+Description of setup()
+*/
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+
+  let data = JSON.parse(localStorage.getItem(`spy-profile-data`));
+  if(data !== null){
+    let password = prompt(`ENTER PASSWORD`);
+
+    if(password === data.password){
+      spyProfile.name = data.name;
+      spyProfile.alias = data.alias;
+      spyProfile.secretWeapon = data.secretWeapon;
+      spyProfile.password = data.password;
+    }
+  }
+  else{
+    generateSpyProfile();
+  }
+
+}
+
+function generateSpyProfile(){
+  // name prompt
+  spyProfile.name = prompt(`What's your name?`);
+
+  // random instrument/alias generator
+  let instrument = random(instrumentData.instruments);
+  spyProfile.alias = `The ${instrument}`;
+
+  // random weapon generator
+  spyProfile.secretWeapon = random(objectData.objects);
+
+
+  // random password generator
+  let card = random(tarotData.tarot_interpretations);
+  spyProfile.password = random(card.keywords);
+
+  // Saving profile to local storage
+  localStorage.setItem(`spy-profile-data`,JSON.stringify(spyProfile));
+}
+
+/**
+Description of draw()
+*/
+function draw() {
+  background(0);
+
+  let profile = `** SPY PROFILE! TOP SECRET **
+
+  NAME: ${spyProfile.name}
+  ALIAS: ${spyProfile.alias}
+  SECRET WEAPON: ${spyProfile.secretWeapon}
+  PASSWORD: ${spyProfile.password}`;
+
+  push();
+  textFont(`Courier, monospace`);
+  textSize(32);
+  textAlign(LEFT,TOP);
+  fill(0, 255, 0);
+  text(profile, 100,100);
+  pop();
+}
