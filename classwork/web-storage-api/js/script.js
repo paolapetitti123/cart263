b7d660d9063b0b25405138352311510e9eb5aaeb
paolap.98 <paolapetitti123@gmail.com>
2021-02-08 21:27:26 -0500

    CW: finished web storage video

--- classwork/web-storage-api/js/script.js
+++ classwork/web-storage-api/js/script.js
@@ -1,22 +1,45 @@
 "use strict";
 
 /**************************************************
-Template p5 project
-Pippin Barr
+4.2. Web Storage API
+Paola Petitti
 
-Here is a description of this template p5 project.
+Following along Web storage api video
 **************************************************/
+let userData = {
+  name: `stranger`
+};
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
+  createCanvas(windowWidth,windowHeight);
 
+  let data = JSON.parse(localStorage.getItem(`web-storage-example`));
+
+  if(data !== null){
+    userData.name = data.name;
+  }
+  else {
+    userData.name = prompt(`What's your name?`);
+    localStorage.setItem(`web-storage-example`, JSON.stringify(userData));
+  }
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
+  background(255);
+
+  push();
+  textSize(64);
+  textAlign(CENTER,CENTER);
+  textStyle(BOLD);
+  fill(0);
+  text(`hey, ${userData.name}!`,width/2,height/2);
+  pop();
+
 
 }
