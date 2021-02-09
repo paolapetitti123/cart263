43b1ef41b4479b4be0ac6f9e76742335982c0572
paolap.98 <paolapetitti123@gmail.com>
2021-02-08 20:13:16 -0500

    CW: finished json video

--- classwork/JSON/js/script.js
+++ classwork/JSON/js/script.js
@@ -4,19 +4,33 @@
 4.1. JSON
 Paola Petitti
 
-Following along the JSON video 
+Following along the JSON video
 **************************************************/
+let jokeText = ``; // The current joke.
+let jokeData = undefined; // The loaded joke data
 
-// setup()
-//
-// Description of setup() goes here.
-function setup() {
-
+function preload() {
+  jokeData = loadJSON(`https://official-joke-api.appspot.com/jokes/programming/random`);
 }
 
-// draw()
-//
-// Description of draw() goes here.
-function draw() {
+function setup() {
+  createCanvas(windowWidth, windowHeight);
 
+  // We get the joke object as the first element of the array
+  let joke = jokeData[0];
+  // Set the joke text as the setup and punchline properties together
+  jokeText = `${joke.setup}\n\n${joke.punchline}`;
+}
+
+function draw() {
+  background(0);
+
+  // Display the current joke
+  push();
+  fill(255, 255, 0);
+  textSize(32);
+  textAlign(CENTER, CENTER);
+  rectMode(CENTER);
+  text(jokeText, width / 2, height / 2, width / 2, height / 2);
+  pop();
 }
