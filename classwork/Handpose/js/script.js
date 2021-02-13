9dbb1c4c9548fc348f4535d31ff0a3955c628e40
paolap.98 <paolapetitti123@gmail.com>
2021-02-13 14:33:39 -0500

    CW: started handpose video

---
+++ classwork/Handpose/js/script.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/**************************************************
+5.3. ml5.js: Handpose
+Paola Petitti
+
+Following Pippin's video.
+**************************************************/
+
+// setup()
+//
+// Description of preload() goes here.
+function preload(){
+
+}
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
