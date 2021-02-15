294c9b948f0f65bf1c76291bd698b21beb8cff27
paolap.98 <paolapetitti123@gmail.com>
2021-02-15 11:02:50 -0500

    A4: Starting Bubble popper activity

---
+++ activities/bubble-popper-activity/js/script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+/**************************************************
+Bubble Popper Activity
+Paola Petitti
+
+Following Pippin's video for the activity.
+**************************************************/
+
+/*
+Description of preload() goes here.
+*/
+function preload(){
+
+}
+
+
+/*
+Description of setup() goes here.
+*/
+function setup() {
+
+}
+
+/*
+Description of draw() goes here.
+*/
+function draw() {
+
+}
