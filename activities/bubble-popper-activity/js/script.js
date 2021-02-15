1e9d7acdb6f48db075c545568159ef9a47ac4b60
paolap.98 <paolapetitti123@gmail.com>
2021-02-15 11:31:37 -0500

    A4: Finished setup

--- activities/bubble-popper-activity/js/script.js
+++ activities/bubble-popper-activity/js/script.js
@@ -7,12 +7,37 @@ Paola Petitti
 Following Pippin's video for the activity.
 **************************************************/
 
+// Webcam of the user
+let video = undefined;
+
+// The Handpose model
+let handpose = undefined;
+
+// The Current set of predictions
+let predictions = [];
 
 /*
 Description of setup() goes here.
 */
 function setup() {
+  createCanvas(640,480);
 
+  // Accessing users webcam
+  video = createCanvas(VIDEO);
+  video.hide();
+
+  // Loading handpose model
+  handpose = ml5.handpose(video,
+    {flipHorizontal: true},
+    function(){
+    console.log(`Model Loaded`);
+  });
+
+  // Listen for predictions
+  handpose.on(`predict`, function (results) {
+    console.log(results);
+    predictions = results;
+  });
 }
 
 /*
