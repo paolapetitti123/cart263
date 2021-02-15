f4501c0c12922fe353723491c1bd11d72905847d
paolap.98 <paolapetitti123@gmail.com>
2021-02-15 11:38:56 -0500

    A4: Got line for index finger to show up

--- activities/bubble-popper-activity/js/script.js
+++ activities/bubble-popper-activity/js/script.js
@@ -44,5 +44,23 @@ function setup() {
 Description of draw() goes here.
 */
 function draw() {
+  background(0);
 
+  if(predictions.length > 0){
+    let hand = predictions[0];
+    let index = hand.annotations.indexFinger;
+    let tip = index[3];
+    let base = index[0];
+    let tipX = tip[0];
+    let tipY = tip[1];
+    let baseX = base[0];
+    let baseY = base[1];
+
+    push();
+    noFill();
+    stroke(255,255,255);
+    strokeWeight(2);
+    line(baseX,baseY,tipX,tipY);
+    pop();
+  }
 }
