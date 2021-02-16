a9cb77b8c977efb8c79316ce87c6731ef144006e
paolap.98 <paolapetitti123@gmail.com>
2021-02-15 20:43:28 -0500

    P1: Made project folder for project 1

---
+++ project-1/flubber-project-1/js/script.js
@@ -0,0 +1,32 @@
+"use strict";
+
+/**************************************************
+Project 1: Flubber Game
+Paola Petitti
+
+For my project, I am going to create a little game based
+on the Disney movie Flubber. The functionality will be somewhat
+similar to the bubble popper activity as the goal of the game
+will be to catch flubber using handpose.
+**************************************************/
+
+/*
+  Description of preload() goes here
+*/
+function preload() {
+
+}
+
+/*
+  Description of setup() goes here
+*/
+function setup() {
+
+}
+
+/*
+  Description of draw() goes here
+*/
+function draw() {
+
+}
