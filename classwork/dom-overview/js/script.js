bd2cf0469e02396f028ce92f6358699b3264f312
paolap.98 <paolapetitti123@gmail.com>
2021-03-08 23:11:15 -0500

    CW: Started DOM notes

---
+++ classwork/dom-overview/js/script.js
@@ -0,0 +1,14 @@
+console.log(document);
+
+// // Get the <h1> element with id "main-heading" and store it in a variable
+// let mainHeading = document.getElementById(`main-heading`);
+// // Set its color property
+// mainHeading.style.color = `#339966`;
+
+// Get the <h1> element with id "main-heading" and store it in a variable
+let mainHeading = document.getElementById(`main-heading`);
+// Change some of the heading's CSS properties
+mainHeading.style.color = `#339966`;
+mainHeading.style.fontSize = `5rem`;
+mainHeading.style.fontFamily = `Helvetica, sans-serif`;
+mainHeading.style.backgroundColor = `#000000`;
