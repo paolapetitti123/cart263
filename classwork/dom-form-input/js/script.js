8cd7904894706d2a3006c621d1b69b4538227e6f
paolap.98 <paolapetitti123@gmail.com>
2021-03-09 12:55:09 -0500

    CW: Finished form input notes

--- classwork/dom-form-input/js/script.js
+++ classwork/dom-form-input/js/script.js
@@ -1,6 +1,10 @@
-let mainHeading = document.getElementById(`main-heading`);
-let originalText = paragraph.innerText;
+let loginButton = document.getElementById(`login-button`);
+let passwordInput = document.getElementById(`password-input`);
+let slider = document.getElementById(`range-slider`);
+let colorPicker = document.getElementById(`color-picker`);
+let datePicker = document.getElementById(`date-picker`);
 
-window.addEventListener(`offline`, function(event) {
-  mainHeading.innerText = `:(`;
+datePicker.addEventListener(`change`, function(event) {
+    let date = event.target.value;
+    alert(date);
 });
