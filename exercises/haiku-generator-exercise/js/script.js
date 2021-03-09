5214e398510bc7272553c7765a0209172f143ff5
paolap.98 <paolapetitti123@gmail.com>
2021-03-09 15:35:33 -0500

    E5: Added randomly generated title, tiled background that changes when you click a button (backgrounds different depending on which title you get), the haiku is hidden until you click the button and I fixed up the CSS

--- exercises/haiku-generator-exercise/js/script.js
+++ exercises/haiku-generator-exercise/js/script.js
@@ -6,7 +6,7 @@ Does what you think it would by the title.
 */
 
 "use strict";
-
+// Arrays for the haiku lines
 let fiveSyllableLines = [
   `O, to be a tree`,
   `The cat does not know`,
@@ -14,7 +14,6 @@ let fiveSyllableLines = [
   `You have done your best`,
   `They are all gone now`
 ];
-
 let sevenSyllableLines = [
   `Say the things left unsaid`,
   `Never believe the wind's lies`,
@@ -22,7 +21,6 @@ let sevenSyllableLines = [
   `Nothing can satisfy you`,
   `They will not come back again`
 ];
-
 let titleLine = [
   `Sword of Victory`,
   `Danger`,
@@ -31,24 +29,66 @@ let titleLine = [
   `Catching Fire`
 ];
 
+
+// Text related variable declarations
 let line1 = random(fiveSyllableLines);
 let line2 = random(sevenSyllableLines);
 let line3 = random(fiveSyllableLines);
 let haikuTitle = random(titleLine);
 
+// Getting the elements from the HTML
 let line1P = document.getElementById(`line-1`);
 let line2P = document.getElementById(`line-2`);
 let line3P = document.getElementById(`line-3`);
 let h1Title = document.getElementById(`title`);
 
+// Setting the text to the appropriate variables.
 line1P.innerText = line1;
 line2P.innerText = line2;
 line3P.innerText = line3;
 h1Title.innerText = haikuTitle;
 
+// adding click event listenters to the text to change when clicked.
 line1P.addEventListener(`click`, lineClicked);
 line2P.addEventListener(`click`, lineClicked);
 line3P.addEventListener(`click`, lineClicked);
+
+// button to change the background
+let button = document.getElementById(`background-button`);
+let textVisible = document.getElementById("haiku");
+textVisible.style.display = "none";
+
+button.addEventListener(`click`,function(event) {
+  if(haikuTitle === `Quasi Una Fantasia`){
+    document.body.background = "assets/images/cherryblossom.gif";
+    document.body.style[`background-color`] = `#b3fffb`;
+    textVisible.style.display = "block";
+  }
+  else if(haikuTitle === `Sword of Victory`){
+    document.body.background = "assets/images/sword.gif";
+    document.body.style[`background-color`] = `#000000`;
+    document.body.style[`color`] = `#ffffff`;
+    textVisible.style.display = "block";
+  }
+  else if(haikuTitle === `Danger`){
+    document.body.background = "assets/images/danger.gif";
+    document.body.style[`background-color`] = `#eb8c34`;
+    document.body.style[`color`] = `#ffffff`;
+    textVisible.style.display = "block";
+  }
+  else if(haikuTitle === `Checkmate`){
+    document.body.background = "assets/images/chess.gif";
+    document.body.style[`background-color`] = `#000000`;
+    document.body.style[`color`] = `#ffffff`;
+    textVisible.style.display = "block";
+  }
+  else if(haikuTitle === `Catching Fire`){
+    document.body.background = "assets/images/fire.gif";
+    document.body.style[`background-color`] = `#000000`;
+    document.body.style[`color`] = `#ffffff`;
+    textVisible.style.display = "block";
+  }
+});
 
 function lineClicked(event){
 
