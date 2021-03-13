211146ea714eea798520ea04b92c7d7940d1d87e
paolap.98 <paolapetitti123@gmail.com>
2021-03-13 15:17:39 -0500

    CW: starting jquery overview

---
+++ classwork/jQuery/js/script.js
@@ -0,0 +1,11 @@
+/**
+Following along jQuery video
+Paola Petitti
+*/
+
+"use strict";
+
+// Get the <h1> element by its id and store it in a variable
+let $mainHeading = $(`#main-heading`);
+// Set its color property using the .css() method
+$mainHeading.css(`color`, `#339966`);
