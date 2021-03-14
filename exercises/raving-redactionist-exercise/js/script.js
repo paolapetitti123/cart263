e2927507674cfc4544825cac9c6c7a3e18cd9a4b
paolap.98 <paolapetitti123@gmail.com>
2021-03-13 21:36:48 -0500

    E5: Started the exercise, changed the text to the declaration of independance and added a scroll image to my image folder

---
+++ exercises/raving-redactionist-exercise/js/script.js
@@ -0,0 +1,27 @@
+/**
+Raving Redactionist Activity
+Paola Petitti
+
+Following along the activity video
+*/
+
+"use strict";
+$(`.top-secret`).on(`click`,redact);
+setInterval(revelation, 500);
+
+function redact(event){
+  $(this).removeClass(`revealed`);
+  $(this).addClass(`redacted`);
+}
+
+function revelation(){
+  $(`.redacted`).each(attemptReveal);
+}
+
+function attemptReveal(){
+  let r = Math.random();
+  if(r < 0.1){
+    $(this).removeClass(`redacted`);
+    $(this).addClass(`revealed`);
+  }
+}
