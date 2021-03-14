817b4b58218bae46418007fdeb174f7e18cd56c7
paolap.98 <paolapetitti123@gmail.com>
2021-03-13 23:02:04 -0500

    A5: Adding a little bit of p5js to have an intro screen

--- exercises/raving-redactionist-exercise/js/script.js
+++ exercises/raving-redactionist-exercise/js/script.js
@@ -4,24 +4,58 @@ Paola Petitti
 
 Following along the activity video
 */
-
 "use strict";
-$(`.top-secret`).on(`click`,redact);
-setInterval(revelation, 500);
+let bgMusic = undefined;
+let state = `start`;
+
+function preload(){
+  bgMusic = loadSound(`assets/sounds/HOMERUNOrchestra.mp3`);
+}
+
+function setup(){
+  createCanvas(windowWidth,windowHeight);
+  bgMusic.setVolume(0.5);
+}
+
+function draw(){
+  if(state === `start`){
+    intro();
+  }
+  else if(state === `game`){
+    game();
+  }
+  else if(state === `win`){
+    win();
+  }
+  else if(state === `lose`){
+    lose();
+  }
+}
+
+function intro(){
+  $(`#secret-document`).hide();
+  textSize(25);
+  textAlign(CENTER);
+  textFont("monospace");
+  text(`REVEAL & STEAL THE DECLARATION OF INDEPENDANCE!
+    PRESS ENTER TO START`, width/2, height/2);
+}
+
+function keyPressed(){
+  if(keyCode === ENTER){
+    state = `game`;
+  }
+}
+
+function game(){
+  remove();
+  $(`#secret-document`).show();
+  $(`.top-secret`).on(`click`,redact);
+
+}
+
 
 function redact(event){
-  $(this).removeClass(`revealed`);
-  $(this).addClass(`redacted`);
-}
-
-function revelation(){
-  $(`.redacted`).each(attemptReveal);
-}
-
-function attemptReveal(){
-  let r = Math.random();
-  if(r < 0.1){
-    $(this).removeClass(`redacted`);
-    $(this).addClass(`revealed`);
-  }
+  $(this).removeClass(`redacted`);
+  $(this).addClass(`revealed`);
 }
