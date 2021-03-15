603dab0322719d7f39b9b9ece13176072c30185b
paolap.98 <paolapetitti123@gmail.com>
2021-03-15 16:23:56 -0400

    P2: Found a library to do a typewriter effect for my intro

--- project-2/prototype/js/script.js
+++ project-2/prototype/js/script.js
@@ -1,5 +1,5 @@
 "use strict";
-
+// import Typewriter from 'typewriter-effect/dist/core';
 /**************************************************
 Hidden Object Game Prototype (Project 2)
 Paola Petitti
@@ -12,36 +12,54 @@ there won't be many objects to find, I'm merely going to get the functionality w
 let counter = 0;
 let titleTxt = `TREASURE`;
 let speed = 500;
+let i = 0;
+let j = 0;
 
-let story = document.getElementById('story');
-let typewriter = new TypeWriter(story, {
-  loop:true
+const textDisplay = document.getElementById('title');
+const phrases = [
+  'People want it',
+  'People dream about it',
+  'It can be different to every individual',
+  'It can complete us',
+  'Or it can destroy us',
+  'And it can change the world',
+  `People call it 'treasure'`,
+  `The sound of wind blowing from the horizon`,
+  `The warmth of the sun hitting the ocean waves`,
+  `The vibration of sand beating like the hearts of youth`,
+  `We're at the starting point of this long journey`,
+  `The freezing winds may make us shiver`,
+  `The heat of the sun may make us thirsty`,
+  `The vibrations of the sand may swallow us`,
+  `But we'll never stop`,
+  `Gold, eternal life, honor, love, fame`,
+  `It doesn't matter what you dream for`,
+  `So let me ask you`,
+  `What is your treasure?`,
+  `Will you join us?`];
+
+
+$(document).ready(function(){
+  $(`#story`).typewrite({
+    actions: [
+      {delay: 11500},
+      {type: 'People want it'},
+      {type: '<br>'},
+      {delay: 2000},
+      {type: 'People dream about it'},
+      {speed: 25},
+      {select: {from: 0, to: 38}},
+      {remove: {num: 38, type: 'whole'}}
+    ]
+  });
 });
 
-typewriter.typeString('Hello World!')
-    .pauseFor(2500)
-    .deleteAll()
-    .typeString('Strings can be removed')
-    .pauseFor(2500)
-    .deleteChars(7)
-    .typeString('<strong>altered!</strong>')
-    .pauseFor(2500)
-    .start();
-
-function typeWriter(){
-  if(counter < titleTxt.length){
-    document.getElementById("title").innerHTML += titleTxt.charAt(counter);
-    counter++;
-    setTimeout(typeWriter, speed);
-  }
-}
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
-  typeWriter();
-$(`#musicPlayer`).hide();
+  $(`#musicPlayer`).hide();
 }
 
 // draw()
