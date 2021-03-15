fecf242637d84727131a329a6f54c68bb9d3af22
paolap.98 <paolapetitti123@gmail.com>
2021-03-15 10:44:09 -0400

    P2: Setup project folder for prototype

---
+++ project-2/prototype/js/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/**************************************************
+Hidden Object Game Prototype (Project 2)
+Paola Petitti
+
+This is a prototype for my final project. This prototype will contain a start screen
+where it's actually a html page with jQuery that provides a backstory for the game,
+once you get through the backstory you'll be able to play the game. For this prototype
+there won't be many objects to find, I'm merely going to get the functionality working.
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
