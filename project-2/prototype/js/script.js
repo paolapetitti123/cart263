6241c74722014125b6550734078e1dc75715901d
paolap.98 <paolapetitti123@gmail.com>
2021-03-15 15:09:08 -0400

    P2: got a basic typewriter effect going on, will change it so i can reuse it multiple times

--- project-2/prototype/js/script.js
+++ project-2/prototype/js/script.js
@@ -9,12 +9,39 @@ where it's actually a html page with jQuery that provides a backstory for the ga
 once you get through the backstory you'll be able to play the game. For this prototype
 there won't be many objects to find, I'm merely going to get the functionality working.
 **************************************************/
+let counter = 0;
+let titleTxt = `TREASURE`;
+let speed = 500;
+
+let story = document.getElementById('story');
+let typewriter = new TypeWriter(story, {
+  loop:true
+});
+
+typewriter.typeString('Hello World!')
+    .pauseFor(2500)
+    .deleteAll()
+    .typeString('Strings can be removed')
+    .pauseFor(2500)
+    .deleteChars(7)
+    .typeString('<strong>altered!</strong>')
+    .pauseFor(2500)
+    .start();
+
+function typeWriter(){
+  if(counter < titleTxt.length){
+    document.getElementById("title").innerHTML += titleTxt.charAt(counter);
+    counter++;
+    setTimeout(typeWriter, speed);
+  }
+}
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
-
+  typeWriter();
+$(`#musicPlayer`).hide();
 }
 
 // draw()
