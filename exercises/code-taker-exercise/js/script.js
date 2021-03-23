49dedd55e833f08d87ce5454ffffc2572b24bde3
paolap.98 <paolapetitti123@gmail.com>
2021-03-23 13:36:55 -0400

    E6: starting code taker exercise

---
+++ exercises/code-taker-exercise/js/script.js
@@ -0,0 +1,38 @@
+/**
+Code Taker Exercise
+Paola Petitti
+
+Following along activity video
+*/
+
+"use strict";
+
+$(`#solved-dialog`).dialog({
+  autoOpen: false,
+  buttons: {
+    "I know.": function(){
+      $(this).dialog(`close`);
+    }
+  }
+});
+
+$(`.secret`).one(`mouseover`, function(event){
+  $(this).addClass(`found`,500);
+  $(this).draggable({
+    helper: `clone`
+  });
+});
+
+$(`#answer`).droppable({
+  drop: function(event,ui){
+    let character = ui.draggable.text();
+    $(this).append(character);
+    ui.draggable.draggable(`disable`);
+    ui.draggable.removeClass(`found`);
+
+    // Check if they got it right
+    if($(this).text() === `Theremin`){
+      $(`#solved-dialog`).dialog(`open`);
+    }
+  }
+});
