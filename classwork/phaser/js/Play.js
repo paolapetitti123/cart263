6103e85a1227e3d46d597502376d0826dfab662c
paolap.98 <paolapetitti123@gmail.com>
2021-03-30 11:46:42 -0400

    CW: finished setup video

---
+++ classwork/phaser/js/Play.js
@@ -0,0 +1,22 @@
+class Play extends Phaser.Scene {
+  constructor(){
+    super({
+      key: `play`
+    });
+  }
+
+  create(){
+    let style = {
+      fontFamily: 'sans-serif',
+      fontSize: `40px`,
+      color: '#ffffff'
+    }
+    let gameDescription = `Think of a number... wrong!`;
+    this.add.text(100,100,gameDescription,style);
+  }
+
+  update(){
+    
+  }
+
+}
---
+++ classwork/phaser/js/main.js
@@ -0,0 +1,20 @@
+/**
+Phaser
+Paola Petitti
+
+Following along the phaser videos
+*/
+
+"use strict";
+
+let config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: `arcade`
+  },
+  scene: [Boot,Play]
+};
+
+let game = new Phaser.Game(config);
