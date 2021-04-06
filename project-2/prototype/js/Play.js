23acd835bbf70552f4cfdfaa63351c11ff04b828
paolap.98 <paolapetitti123@gmail.com>
2021-04-06 13:22:15 -0400

    P2: Got the animations working

--- project-2/prototype/js/Play.js
+++ project-2/prototype/js/Play.js
@@ -9,9 +9,12 @@ class Play extends Phaser.Scene {
     this.background = this.add.image(640,360,`background`);
 
 
-    this.avatar = this.add.sprite(400,500,`avatar`).setScale(2.5,2.5);
+    this.avatar = this.physics.add.sprite(400,500,`avatar`).setScale(2.5,2.5);
+    this.avatar.setCollideWorldBounds(true);
     this.createAnimations();
     this.avatar.play(`avatar-idle`);
+
+    this.cursors = this.input.keyboard.createCursorKeys();
   }
 
   createAnimations(){
@@ -25,7 +28,7 @@ class Play extends Phaser.Scene {
       repeat: -1
     });
     this.anims.create({
-      key: `avatar-moving-left `,
+      key: `avatar-moving-left`,
       frames: this.anims.generateFrameNumbers(`avatar`, {
         start: 9,
         end: 17
@@ -41,7 +44,8 @@ class Play extends Phaser.Scene {
       }),
       frameRate: 12,
       repeat: -1
-    });this.anims.create({
+    });
+    this.anims.create({
       key: `avatar-moving-right`,
       frames: this.anims.generateFrameNumbers(`avatar`, {
         start: 27,
@@ -62,6 +66,57 @@ class Play extends Phaser.Scene {
   }
 
   update(){
+    this.handleInput();
+  }
+
+  handleInput(){
+    // Left & right arrow keys
+    if(this.cursors.left.isDown){
+      this.avatar.setVelocityX(-100);
+    }
+    else if (this.cursors.right.isDown){
+      this.avatar.setVelocityX(100);
+    }
+    else {
+      this.avatar.setVelocityX(0);
+    }
+
+    // Up & Down arrow keys
+    if(this.cursors.up.isDown){
+      this.avatar.setVelocityY(-100);
+    }
+    else if(this.cursors.down.isDown){
+      this.avatar.setVelocityY(100);
+    }
+    else {
+      this.avatar.setVelocityY(0);
+    }
+
+    // Changing the animations (L,R,U,D)
+    if(this.avatar.body.velocity.x !== 0 || this.avatar.body.velocity.y !== 0){
+      if(this.avatar.body.velocity.x < 0){
+        this.avatar.play(`avatar-moving-left`, true);
+      }
+      else if(this.avatar.body.velocity.x > 0){
+        this.avatar.play(`avatar-moving-right`, true);
+      }
+      else if(this.avatar.body.velocity.x < 0 && (this.avatar.body.velocity.y < 0 || this.avatar.body.velocity.y > 0)){
+        this.avatar.play(`avatar-moving-left`, true);
+      }
+      else if(this.avatar.body.velocity.x > 0 && (this.avatar.body.velocity.y < 0 || this.avatar.body.velocity.y > 0)){
+        this.avatar.play(`avatar-moving-right`, true);
+      }
+      if(this.avatar.body.velocity.y < 0){
+        this.avatar.play(`avatar-moving-up`,true);
+      }
+      else if(this.avatar.body.velocity.y > 0){
+        this.avatar.play(`avatar-moving-down`,true);
+      }
+    }
+    // Default animation
+    else {
+      this.avatar.play(`avatar-idle`, true);
+    }
 
   }
 }
